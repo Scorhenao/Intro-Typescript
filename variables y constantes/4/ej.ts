7956d46773216e0de5b3e784412737a064766fa5
Scorhenao <samcorhenao@gmail.com>
2024-07-30 21:21:57 -0400

    question of ej 4 was missed

--- variables y constantes/4/ej.ts
+++ variables y constantes/4/ej.ts
@@ -8,6 +8,14 @@ function imprimirNombres(nombres: string[]): void {
 
 /**
  * ¿Qué ventajas tiene el método map sobre un bucle for en TypeScript?
- * 
+ * El método map tiene las siguientes ventajas sobre un bucle for en TypeScript:
+
+    Concisión: map permite transformar arrays en una sola línea de código, haciendo el código más limpio y legible.
+
+    Inmutabilidad: map no modifica el array original, sino que devuelve un nuevo array con los elementos transformados.
+
+    Declarativo: map expresa claramente la intención de aplicar una transformación a cada elemento, en contraste con el enfoque imperativo de un bucle for.
+
+    Funcionalidad: map es parte de la programación funcional y se integra bien con otros métodos de array como filter y reduce.
  * 
  */
