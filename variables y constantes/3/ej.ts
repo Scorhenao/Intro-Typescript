f8d66acdcc593b53d700f700119cf8ac711af543
Scorhenao <samcorhenao@gmail.com>
2024-07-30 21:10:16 -0400

    ej 3 variables y constantes READY

--- variables y constantes/3/ej.ts
+++ variables y constantes/3/ej.ts
@@ -1,1 +1,27 @@
 //Implementa una función que clasifique un número en positivo, negativo o cero usando if y switch.
+let numero: number = 2;
+
+if (numero > 0) {
+    console.log("El número es positivo");
+}else if(numero < 0) {
+    console.log("El número es negativo");
+}else{
+    console.log("El número es cero");
+}
+
+switch (true) {
+    case numero > 0:
+        console.log("El número es positivo");
+        break;
+        case numero < 0:
+            console.log("El número es negativo");
+            break;
+            default:
+                console.log("El número es cero");
+                break;
+                }
+
+//¿Cuándo es preferible usar un operador ternario en lugar de una estructura if?
+/**
+ * 
+ */
